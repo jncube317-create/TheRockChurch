64acf14af15694e510a443aa18e449ef37845efe
Joshua <jncube317@gmail.com>
2026-02-24 13:25:49 +0200

    Initial deploy: src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+window.onerror = function (message, source, lineno, colno, error) {
+  alert('Global Error: ' + message + '\nAt: ' + source + ':' + lineno);
+};
+
+console.log('App starting...');
+console.log('Supabase URL present:', !!import.meta.env.VITE_SUPABASE_URL);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
